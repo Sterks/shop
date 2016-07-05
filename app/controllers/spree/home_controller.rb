b2fbe7787388cb6ac4e15f5379619ab2f239bdd6
Denis S. Runov <it-saratov@mail.ru>
2016-07-05 22:40:21 +0300

    step2

---
+++ app/controllers/spree/home_controller.rb
@@ -0,0 +1,14 @@
+module Spree
+  class HomeController < Spree::StoreController
+    helper 'spree/products'
+    respond_to :html
+
+    def index
+      @searcher = build_searcher(params.merge(include_images: true))
+      @products = @searcher.retrieve_products.includes(:possible_promotions)
+      @taxonomies = Spree::Taxonomy.includes(root: :children)
+      @pr = Spree::Product.all
+      @first = @pr.first.taxons.where(name: "First").first
+    end
+  end
+end
